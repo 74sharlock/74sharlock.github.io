1f01499a5a17899e7e501f6cf27694ac52da23a0
74sharlock <sharlock7788@gmail.com>
2017-06-26 10:48:48 +0800

    add pictures

---
+++ javascripts/picture-data.js
@@ -0,0 +1,6 @@
+window.pictures = [{
+  name: '城市猎人',
+  prefix: (menu, volume)=>`http://jpgcdn.dmzx.com/img/c%2F%B3%C7%CA%D0%C1%D4%C8%CB%2F${menu}%2FCH${menu}_${volume}.jpg`,
+  author: '北条司',
+  page: 'city_hunter'
+}];
---
+++ javascripts/picture.js
@@ -0,0 +1,115 @@
+(function (Vue, pictureIndex) {
+
+  const {prefix} = pictures[pictureIndex];
+
+  let volume = 1;
+  let section = 1;
+
+  let progress = localStorage.getItem('progress');
+
+  progress = progress ? JSON.parse(progress) : {};
+
+  if(progress[pictureIndex]){
+    let picture = progress[pictureIndex].split(',');
+    volume = Number(picture[0].trim());
+    section = Number(picture[1].trim());
+  }
+
+  let app = new Vue({
+    data: {
+      volume,
+      section,
+      src: '',
+      notFound: false
+    },
+    methods: {
+      prev(){
+        this.section = this.section > 1 ? (this.section - 1) : this.section;
+        this.show();
+      },
+      next(){
+        this.section = this.section + 1;
+        this.show();
+      },
+      prevVolume(){
+        this.volume = this.volume > 1 ? (this.volume - 1) : this.volume;
+        this.section = 1;
+        this.show();
+      },
+      nextVolume(){
+        this.volume = this.volume + 1;
+        this.section = 1;
+        this.show();
+      },
+      show(){
+        let img = new Image();
+        img.onload = () => {
+          progress[pictureIndex] = `${this.volume},${this.section}`;
+          localStorage.setItem('progress', JSON.stringify(progress));
+          this.notFound = false;
+          this.src = this.url;
+        };
+        img.onerror = () => {
+          this.src = '';
+          this.notFound = true;
+        };
+        img.src = this.url;
+      }
+    },
+    computed: {
+      m(){
+        return this.volume < 10 ? `0${this.volume}` : this.volume;
+      },
+      v(){
+        let result = '';
+        switch (true){
+          case(this.section > 999):
+            result = this.section;
+            break;
+          case(this.section > 99 && this.section < 1000):
+            result = `0${this.section}`;
+            break;
+          case(this.section > 9 && this.section < 100):
+            result = `00${this.section}`;
+            break;
+          case(this.section < 10):
+            result = `000${this.section}`;
+            break;
+        }
+        return result;
+      },
+      url(){
+        return prefix(this.m, this.v);
+      }
+    },
+    created(){
+      this.show();
+    }
+  });
+
+  app.$mount('#app');
+
+  const keyUpHandler = {
+    '38': function(){
+      app.prevVolume();
+    },
+    '40': function(){
+      app.nextVolume();
+    },
+    '37': function(){
+      app.prev();
+    },
+    '39': function(){
+      app.next();
+    },
+  };
+
+  document.addEventListener('keyup', function (event) {
+    keyUpHandler[event.keyCode] && keyUpHandler[event.keyCode]();
+  }, false);
+
+  document.oncontextmenu = function () {
+    return false;
+  };
+
+})(Vue, pictureIndex);
